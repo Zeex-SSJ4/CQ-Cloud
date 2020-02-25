43bd5f982549c993ecb0ab626168d150ff1bb694
Chloé Vieille <chloevieille@hotmail.com>
2020-02-25 11:06:32 +0100

    Création de l'authentification

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # CQ-Cloud
+
+## Commandes : 
+- composer install
+
+- php artisan serve (start a development server at https://localhost:8000)
+- php artisan list  (see list of all commands)
+- php artisan make:controller <controllerName>
+- php artisan make:model <modelName>
+- php artisan route:list (get routing list)
+php artisan make:model User
+
+
+## Configuration BDD :
+- php artisan migrate:install 
+- php artisan migrate
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +11,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
